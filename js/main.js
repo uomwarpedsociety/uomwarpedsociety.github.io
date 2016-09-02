c5cc4b55c5cba7fea988d511a7164a76abc0ef86
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-02 11:54:51 +0100

    save

---
+++ js/main.js
@@ -0,0 +1,19 @@
+clickabout = function(){
+  document.getElementById('aboutimage').src = "image/aboutselected.png";
+}
+
+clickevent = function(){
+  document.getElementById('eventimage').src = "image/eventsselected.png";
+}
+
+clickpicture = function(){
+  document.getElementById('pictureimage').src = "image/picturesselected.png";
+}
+
+clickjoin = function(){
+  document.getElementById('joinimage').src = "image/joinselected.png";
+}
+
+clickcommittee = function(){
+  document.getElementById('committeeimage').src = "committeeselected.png";
+}
