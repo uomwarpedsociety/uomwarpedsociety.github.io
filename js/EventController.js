0eaf370216562a9d0578d3a1a633e8ef1962ecc4
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-02 11:25:37 +0100

    save

--- js/EventController.js
+++ js/EventController.js
@@ -1,19 +1,13 @@
 
-
 app.controller('EventController', function($scope,$routeParams) {
-
-  $scope.EventUpdate = function(){
-
   $scope.name = 'EventController';
   $scope.params = $routeParams;
   $scope.events = [
-
     {
       date: "xx/xx/xx",
       title: "First Event",
       description: "This is a short description about this event",
       photo: "/image/eventphoto/join.png",
     },
-
   ]
 })
