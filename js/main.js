8be5616570d730e8eed82506f41268339539ec97
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-02 11:58:18 +0100

    save

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
 clickabout = function(){
   document.getElementById('aboutimage').src = "image/aboutselected.png";
+  document.getElementById('eventimage').src = "image/events.png";
+  document.getElementById('pictureimage').src = "image/pictures.png";
+  document.getElementById('joinimage').src = "image/join.png";
+  document.getElementById('committeeimage').src = "image/committee.png";
 }
 
 clickevent = function(){
+  document.getElementById('aboutimage').src = "image/about.png";
   document.getElementById('eventimage').src = "image/eventsselected.png";
+  document.getElementById('pictureimage').src = "image/pictures.png";
+  document.getElementById('joinimage').src = "image/join.png";
+  document.getElementById('committeeimage').src = "image/committee.png";
 }
 
 clickpicture = function(){
+  document.getElementById('aboutimage').src = "image/about.png";
+  document.getElementById('eventimage').src = "image/events.png";
   document.getElementById('pictureimage').src = "image/picturesselected.png";
+  document.getElementById('joinimage').src = "image/join.png";
+  document.getElementById('committeeimage').src = "image/committee.png";
 }
 
 clickjoin = function(){
+  document.getElementById('aboutimage').src = "image/about.png";
+  document.getElementById('eventimage').src = "image/events.png";
+  document.getElementById('pictureimage').src = "image/pictures.png";
   document.getElementById('joinimage').src = "image/joinselected.png";
+  document.getElementById('committeeimage').src = "image/committee.png";
 }
 
 clickcommittee = function(){
-  document.getElementById('committeeimage').src = "committeeselected.png";
+  document.getElementById('aboutimage').src = "image/about.png";
+  document.getElementById('eventimage').src = "image/events.png";
+  document.getElementById('pictureimage').src = "image/pictures.png";
+  document.getElementById('joinimage').src = "image/join.png";
+  document.getElementById('committeeimage').src = "image/committeeselected.png";
 }
