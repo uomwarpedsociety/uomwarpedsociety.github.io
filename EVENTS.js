c60f1fb89e06e8ea9ba646edf15afe90526c99ae
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-17 15:40:12 +0100

    save

--- EVENTS.js
+++ EVENTS.js
@@ -5,14 +5,16 @@ app.controller('EventController', function($scope,$routeParams) {
     {
       date: "26/09/2016",
       title: "Quiz Night and Welcome Event",
-      description: "Our first event of the year! This will be a pub quiz night followed by general meet and greet with everyone to get to know all of you lovely folks! /br",
+      description: "Our first event of the year! This will be a pub quiz night followed by general meet and greet with everyone to get to know all of you lovely folks! /br Where: Sandbar,",
       photo: "/image/eventphoto/quiz1.jpg",
+      facebooklink: "",
     },
     {
       date: "28/09/2016",
       title: "Miss Peregrine's Home for Peculiar Children",
-      description: "This is a short description about this event",
+      description: "When Jacob discovers clues to a mystery that spans different worlds and times, he finds Miss Peregrine's Home for Peculiar Children. But the mystery and danger deepen as he gets to know the residents and learns about their special powers. /br Our first Cinema trip of the year. /br Where: The AMC Manchester",
       photo: "/image/eventphoto/quiz1.jpg",
+      facebooklink: "",
     },
   ]
 })
