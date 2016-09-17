0cce7e7201ffa669f066979020b724f3e272d6bd
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-17 17:24:17 +0100

    save

--- EVENTS.js
+++ EVENTS.js
@@ -12,7 +12,7 @@ app.controller('EventController', function($scope,$routeParams) {
     {
       date: "02/10/2016",
       title: "Miss Peregrine's Home for Peculiar Children",
-      description: "When Jacob discovers clues to a mystery that spans different worlds and times, he finds Miss Peregrine's Home for Peculiar Children. But the mystery and danger deepen as he gets to know the residents and learns about their special powers. Our first Cinema trip of the year. Where: The AMC Manchester, When: 2:30pm"
+      description: "When Jacob discovers clues to a mystery that spans different worlds and times, he finds Miss Peregrine's Home for Peculiar Children. But the mystery and danger deepen as he gets to know the residents and learns about their special powers. Our first Cinema trip of the year. Where: The AMC Manchester, When: 2:30pm",
       photo: "/image/eventphoto/peculiar.jpg",
       url: "https://www.facebook.com/events/225160511220111/",
     },
