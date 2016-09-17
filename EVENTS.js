b55e1eb9f0d23a3583c617c19797ce15664d24e1
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-17 15:37:51 +0100

    save

--- EVENTS.js
+++ EVENTS.js
@@ -3,10 +3,16 @@ app.controller('EventController', function($scope,$routeParams) {
   $scope.params = $routeParams;
   $scope.events = [
     {
-      date: "xx/xx/xx",
-      title: "First Event",
+      date: "26/09/2016",
+      title: "Quiz Night and Welcome Event",
+      description: "Our first event of the year! This will be a pub quiz night followed by general meet and greet with everyone to get to know all of you lovely folks! /br",
+      photo: "/image/eventphoto/quiz1.jpg",
+    },
+    {
+      date: "28/09/2016",
+      title: "Miss Peregrine's Home for Peculiar Children",
       description: "This is a short description about this event",
-      photo: "/image/eventphoto/",
+      photo: "/image/eventphoto/quiz1.jpg",
     },
   ]
 })
