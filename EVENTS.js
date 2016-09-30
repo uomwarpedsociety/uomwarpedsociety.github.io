261b59ce2d09abb8f695fff6444edd24c2425339
dylan0150 <dylan.hanner@yahoo.co.uk>
2016-09-30 03:15:32 +0100

    Event Update

--- EVENTS.js
+++ EVENTS.js
@@ -3,15 +3,6 @@ app.controller('EventController', function($scope,$routeParams) {
   $scope.params = $routeParams;
   $scope.events = [
     {
-      date: "26/09/2016",
-      title: "Quiz Night and Welcome Event",
-      description: "Our first event of the year! This will be a pub quiz night followed by general meet and greet with everyone to get to know all of you lovely folks!",
-      photo: "/image/eventphoto/quizavengers.jpg",
-      url: "https://www.facebook.com/events/178364632600307/",
-      time: "7:30pm",
-      location: "Sandbar (M1 7HL)",
-    },
-    {
       date: "02/10/2016",
       title: "Miss Peregrine's Home for Peculiar Children",
       description: "When Jacob discovers clues to a mystery that spans different worlds and times, he finds Miss Peregrine's Home for Peculiar Children. But the mystery and danger deepen as he gets to know the residents and learns about their special powers. Our first Cinema trip of the year.",
@@ -67,6 +58,15 @@ app.controller('EventController', function($scope,$routeParams) {
     },
   ];
   $scope.pastevents = [
+    {
+      date: "26/09/2016",
+      title: "Quiz Night and Welcome Event",
+      description: "Our first event of the year! This will be a pub quiz night followed by general meet and greet with everyone to get to know all of you lovely folks!",
+      photo: "/image/eventphoto/quizavengers.jpg",
+      url: "https://www.facebook.com/events/178364632600307/",
+      time: "7:30pm",
+      location: "Sandbar (M1 7HL)",
+    },
     {
       date: "21/09/2016",
       title: "Jeff Wayne's Musical War of the Worlds ft. RockSoc",
